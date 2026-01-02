411808c6c63a09218b4971c9a2af6b6cc388457a
najijrwan <najijrwanyt@gmail.com>
2026-01-02 15:14:44 +0200

    .

--- src/components/sections/home/HeroSlider.jsx
+++ src/components/sections/home/HeroSlider.jsx
@@ -7,17 +7,14 @@ const HeroSlider = ({ }) => {
             pb-10
             flex flex-col gap-8"
         >
-            <div
-                className="
-                relative
-                w-full h-[304px] 2xl:h-[536px]"
-            >
+            <div class="relative w-full h-[304px] 2xl:h-[536px] overflow-visible">
                 <img
-                    src="src/assets/images/Paste image 2.png"
+                    src="src/assets/images/hero image 1.png"
                     alt=""
-                    className="h-full h-full object-cover object-[60%_60%]"
+                    class="h-full w-full object-cover object-[60%_100%]"
                 />
             </div>
+
 
             <div className=""></div>
         </section>
