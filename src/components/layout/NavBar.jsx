15905b42cbd236c94ea87bd42f5353586125a4a1
najijrwan <najijrwanyt@gmail.com>
2026-01-03 08:09:35 +0200

    .

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -43,7 +43,8 @@ const NavBar = () => {
                 </button>
                 <header
                     className='
-                    text-(--primary) text-base 2xl:text-2xl leading-6 2xl:leading-6 tracking-[0] font-poppins font-medium whitespace-break-spaces'
+                    text-(--primary) text-base 2xl:text-2xl leading-6 2xl:leading-6 tracking-[0] 
+                    font-poppins font-medium whitespace-break-spaces'
                 >
                     3legant<span className="text-(--neutral-4-100)">. </span>
                 </header>
