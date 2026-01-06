bd2f853af1bb0b574eb5a35964169e0a706b7627
najijrwan <najijrwanyt@gmail.com>
2026-01-06 08:33:23 +0200

    .

--- src/components/sections/home/BannerGrid.jsx
+++ src/components/sections/home/BannerGrid.jsx
@@ -4,14 +4,17 @@ const bannerItems = [
     {
         image: 'src/assets/images/product image 4.png',
         category: 'Living Room',
+        height: 'h-[377px]'
     },
     {
         image: 'src/assets/images/product image 5.png',
         category: 'Bedroom',
+        height: 'h-[160px]'
     },
     {
         image: 'src/assets/images/product image 6.png',
         category: 'kitchen',
+        height: 'h-[100px]'
     },
 ]
 
@@ -23,38 +26,18 @@ const BannerGrid = () => {
             px-8 2xl:px-40
             grid grid-cols-1 2xl:grid-cols-2 grid-rows-4 2xl:grid-rows-2 gap-4 2xl:gap-6"
         >
-            <div
-                className="
-                row-span-2
-                bg-n2100"
-            >
-                <img
-                    src={bannerItems[0].image}
-                    alt={`${bannerItems[0].category} image`}
-                    className="h-[377px] object-contain object-bottom"
-                />
-            </div>
-            <div
-                className="
-                bg-n2100"
-            >
-                <img
-                    src={bannerItems[1].image}
-                    alt={`${bannerItems[1].category} image`}
-                    className="h-[160px] object-contain object-bottom"
-                />
-            </div>
-            <div
-                className="
-                
-                bg-n2100"
-            >
-                <img
-                    src={bannerItems[2].image}
-                    alt={`${bannerItems[2].category} image`}
-                    className="h-[100px] object-contain object-bottom"
-                />
-            </div>
+            {bannerItems.map((item, i) => (
+                <div
+                    key={i}
+                    className={`${item.category === 'Living Room' ? 'row-span-2' : ''} bg-n2100`}
+                >
+                    <img
+                        src={item.image}
+                        alt={`${item.category} image`}
+                        className={`${item.height} object-contain object-bottom`}
+                    />
+                </div>
+            ))}
         </section>
     );
 }
