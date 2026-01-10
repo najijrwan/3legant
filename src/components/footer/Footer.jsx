3e924f026a7ad92836ac38bc3a6b6a9c70d790f7
najijrwan <najijrwanyt@gmail.com>
2026-01-10 12:55:52 +0200

    .

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,7 @@
-// components/footer/Footer.jsx
-import React from "react";
-import { NewsLetter } from "@newsletter";
 import { FooterMain } from '@footer';
 
-const Footer = () => (
-    <>
-        <NewsLetter />
-        <FooterMain />
-    </>
-);
+const Footer = () => {
+    return <FooterMain />;
+}
 
 export default Footer;
--- src/components/footer/FooterMain.jsx
+++ src/components/footer/FooterMain.jsx
@@ -6,21 +6,23 @@ const FooterMain = () => {
         <footer
             className="
             w-full px-8 2xl:px-40 pl-8 pt-20 py-12
-            flex flex-col gap-10 gap[49px]
+            flex flex-col gap-10 gap-[49px]
             text-center
             bg-n7100"
         >
             <div
                 className="
-                w-full
+                w-full 2xl:h-8
                 flex flex-col items-center 2xl:flex-row 2xl:justify-between gap-10"
             >
                 <div
                     className="
                     w-full 2xl:w-[544px]
-                    flex flex-col 2xl:flex-row gap-4 2xl:gap-8"
+                    flex flex-col items-center 2xl:flex-row gap-4 2xl:gap-8"
                 >
                     <Brand className='text-white text-2xl' />
+
+                    <div className='w-6 h-px 2xl:h-6 2xl:w-px bg-n4100'></div>
 
                     <p
                         className="
@@ -42,18 +44,18 @@ const FooterMain = () => {
 
             <div
                 className='
-                w-full py-6
-                flex flex-col items-center gap-8'
+                w-full py-6 2xl:py-4
+                flex flex-col 2xl:flex-row-reverse items-center gap-8'
             >
                 <SocialLinks color='n1100' />
 
                 <div
                     className='
                     w-full
-                    flex flex-col items-center gap-6.5'
+                    flex flex-col 2xl:flex-row-reverse 2xl:justify-end items-center gap-7'
                 >
                     <div
-                        className='flex gap-6.5 text-n1100 caption-2-bold'
+                        className='flex gap-[inherit] text-n1100 caption-2-bold'
                     >
                         <p>Privacy Policy</p>
                         <p>Terms of Use</p>
