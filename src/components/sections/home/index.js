d906969f7cb4457e8c4952f215f6ce55716b65a8
najijrwan <najijrwanyt@gmail.com>
2026-01-11 09:26:38 +0200

    .

--- src/components/sections/home/index.js
+++ src/components/sections/home/index.js
@@ -2,7 +2,5 @@ export { default as HeroSlider } from './HeroSlider.jsx';
 export { default as BannerGrid } from './BannerGrid.jsx';
 export { default as NewArrivals } from './NewArrivals.jsx';
 export { default as Values } from './Values.jsx';
-export { VALUES } from './values.js';
 export { default as Banner } from './Banner.jsx';
 export { default as Blog } from './Blog.jsx';
-export { HOME_ARTICLES } from './homeArticles.js';
