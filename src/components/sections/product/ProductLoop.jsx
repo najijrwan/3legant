411ff91b2a41155b67b8abf905f72d78a737c188
najijrwan <najijrwanyt@gmail.com>
2026-01-18 10:22:02 +0200

    .

--- src/components/sections/product/ProductLoop.jsx
+++ src/components/sections/product/ProductLoop.jsx
@@ -199,14 +199,17 @@ const ProductLoop = () => {
                 <div
                     className='
                     w-full pb-6
-                    flex flex-col justify-center gap-2'
+                    flex flex-col gap-2'
                 >
                     <p className='flex gap-[98px]'>
                         <span className='text-n4100 caption-2'>SKU</span>
                         <span className='text-n7100 caption-2'>1117</span>
                     </p>
 
-                    <p>Category</p>
+                    <div className='flex gap-[58px]'>
+                        <p className='text-n4100 caption-2'>Category</p>
+                        <p className='text-n7100 caption-2'>Living Room, Bedroom</p>
+                    </div>
                 </div>
             </div>
         </section>
