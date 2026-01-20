9ff177583db87e116e6d93ed197abf49de83d847
najijrwan <najijrwanyt@gmail.com>
2026-01-20 19:50:57 +0200

    .

--- src/components/sections/product/ProductTabs.jsx
+++ src/components/sections/product/ProductTabs.jsx
@@ -188,7 +188,12 @@ const productTabs = [
 ];
 
 const ProductTabs = () => {
-    const [activeTab, setActiveTab] = useState(null);
+    const [activeTab, setActiveTab] = useState('reviews');
+
+    const activeTabContent = productTabs.find(
+        (tab) => tab.id === activeTab
+    )?.content;
+
     return (
         <section
             className="
@@ -202,38 +207,39 @@ const ProductTabs = () => {
                     const isOpen = activeTab === tab.id;
 
                     return (
-                        <>
-                            <div
-                                key={tab.id}
-                                className={`
+                        <div
+                            key={tab.id}
+                            className={`
                                 pb-2 2xl:pb-0
                                 ${isOpen ? 'border-b border-b-n7100' : 'border-b border-b-n4100 2xl:border-0'}`}
+                        >
+                            <button
+                                onClick={() =>
+                                    setActiveTab(isOpen ? null : tab.id)
+                                }
+                                className="w-full flex items-center justify-between"
                             >
-                                <button
-                                    onClick={() =>
-                                        setActiveTab(isOpen ? null : tab.id)
-                                    }
-                                    className="w-full flex items-center justify-between"
-                                >
-                                    <span className={`text-n4100 ${isOpen ? 'text-n7100' : ''} btn-m`}>{tab.label}</span>
+                                <span className={`text-n4100 ${isOpen ? 'text-n7100' : ''} btn-m`}>{tab.label}</span>
 
-                                    <Icon
-                                        name="ChevronDown"
-                                        spanClassName="size-6 2xl:hidden"
-                                        iconClassName={`
+                                <Icon
+                                    name="ChevronDown"
+                                    spanClassName="size-6 2xl:hidden"
+                                    iconClassName={`
                                         w-[12px] h-[6px]
                                         ${isOpen ? 'rotate-180' : 'rotate-0'}
                                         transition-all duration-150`}
-                                    />
-                                </button>
-                            </div>
-
-                            {isOpen && tab.content}
-                        </>
+                                />
+                            </button>
+                        </div>
                     );
                 })}
-
             </div>
+
+            {activeTabContent && (
+                <>
+                    {activeTabContent}
+                </>
+            )}
         </section>
     );
 }
