3067d08991d598e2ccd6a71f063c58c5953983e9
najijrwan <najijrwanyt@gmail.com>
2026-01-20 09:37:38 +0200

    .

--- src/components/sections/product/ProductTabs.jsx
+++ src/components/sections/product/ProductTabs.jsx
@@ -39,6 +39,40 @@ const productTabs = [
             </div>
         ),
     },
+    {
+        id: 'questions',
+        label: 'Questions',
+        content: (
+            <div className="py-2 flex flex-col gap-4">
+                {ADDITONAL_INFO.map((ele, i) => (
+                    <div key={i} className="flex flex-col gap-2">
+                        <p className="text-n4100 caption-1-semi">
+                            {ele.infoTitle}
+                        </p>
+
+                        <p className="text-n7100 caption-2 whitespace-pre-line">
+                            {ele.infoText}
+                        </p>
+                    </div>
+                ))}
+            </div>
+        ),
+    },
+    {
+        id: "reviews",
+        label: 'Reviews',
+        content: (
+            <>
+                <div className='flex flex-col gap-8'>
+                    <div className='flex flec-col gap-6'>
+                        <p className='text-[#23262F] h7'>Customer Reviews</p>
+
+                        <div className='flex flex-col gap-2'></div>
+                    </div>
+                </div>
+            </>
+        ),
+    }
 ];
 
 const ProductTabs = () => {
