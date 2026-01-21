61227bdcddc878041f1225c0750ef5516d5051bb
najijrwan <najijrwanyt@gmail.com>
2026-01-21 19:05:59 +0200

    .

--- src/components/sections/product/ProductLoop/ProductMedia/variants/DesktopGrid.jsx
+++ src/components/sections/product/ProductLoop/ProductMedia/variants/DesktopGrid.jsx
@@ -1,15 +1,20 @@
+import { Badges } from '@ui';
+
 const DesktopGrid = ({ images }) => {
     return (
         <div className="grid grid-cols-2 grid-row-3 gap-6">
             {images.map((image, i) => (
                 <div
                     key={i}
-                    className='w-[262px] h-[349px]'
+                    className='relative w-[262px] h-[349px] bg-n2100'
                 >
                     <img
                         src={image}
                         alt=""
-                        className="size-full object-cover" />
+                        className="size-full object-cover"
+                    />
+
+                    {i === 0 ? <Badges variant='large' /> : ''}
                 </div>
             ))}
         </div>
