01716218e14a28f8c9ecf311fc532087053bf580
najijrwan <najijrwanyt@gmail.com>
2026-01-21 22:22:51 +0200

    .

--- src/components/sections/product/ProductLoop/ProductMedia/variants/DesktopCarousel.jsx
+++ src/components/sections/product/ProductLoop/ProductMedia/variants/DesktopCarousel.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import { Badges } from '@ui';
 
 const DesktopCarousel = ({ images }) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const handlePrev = () => {
+        setActiveIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+    };
+
+    const handleNext = () => {
+        setActiveIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+    };
+
+
     return (
         <div className='grid grid-cols-3 grid-rows-3'>
             <div
@@ -11,25 +23,41 @@ const DesktopCarousel = ({ images }) => {
                 bg-n2100'
             >
                 <img
-                    src={images[0]}
-                    alt=""
+                    src={images[activeIndex]}
+                    alt={`Product image ${activeIndex + 1}`}
                     className='size-[350px] 2xl:size-[650px] object-cover object-bottom'
                 />
 
                 <Badges variant='large' />
+
+                {/* Left button */}
+                <button
+                    onClick={handlePrev}
+                    className="absolute left-2 top-1/2 -translate-y-1/2 bg-white p-2 rounded-full shadow-md z-10"
+                >
+                    &#8592;
+                </button>
+
+                {/* Right button */}
+                <button
+                    onClick={handleNext}
+                    className="absolute right-2 top-1/2 -translate-y-1/2 bg-white p-2 rounded-full shadow-md z-10"
+                >
+                    &#8594;
+                </button>
             </div>
 
             {images.map((image, i) => (
                 <div
                     key={i}
-                    className='size-[167px]'
+                    className={`size-[167px] cursor-pointer border-2 ${activeIndex === i ? 'border-n7100' : 'border-transparent'
+                        }`}
+                    onClick={() => setActiveIndex(i)}
                 >
-                    <img
-                        src={image}
-                        alt=""
-                        className="size-full" />
+                    <img src={image} alt={`Thumbnail ${i + 1}`} className="size-full" />
                 </div>
             ))}
+
         </div>
     )
 }
