48b053c818fd1583d618cce9a8057321bc31d554
najijrwan <najijrwanyt@gmail.com>
2026-01-21 08:31:20 +0200

    .

--- src/components/sections/product/ProductTabs/ProductTabs.jsx
+++ src/components/sections/product/ProductTabs/ProductTabs.jsx
@@ -11,9 +11,9 @@ const ProductTabs = () => {
 
     return (
         <section
-            className="
+            className={`
             px-8 2xl:px-40 pb-20 2xl:py-10
-            flex flex-col gap-10 2xl:gap-12"
+            flex flex-col ${activeTab === 'additionalInfo' ? 'gap-0' : 'gap-10'} 2xl:gap-12`}
         >
             <div
                 className="w-full flex flex-col 2xl:flex-row 2xl:gap-20"
@@ -25,24 +25,26 @@ const ProductTabs = () => {
                         <div
                             key={tab.id}
                             className={`
-                                pb-2 2xl:pb-0
-                                ${isOpen ? 'border-b border-b-n7100' : 'border-b border-b-n4100 2xl:border-0'}`}
+                            pb-2 2xl:pb-0
+                            ${isOpen ? 'border-b border-b-n7100' : 'border-b border-b-n4100 2xl:border-0'}`}
                         >
                             <button
-                                onClick={() =>
-                                    setActiveTab(isOpen ? null : tab.id)
-                                }
+                                onClick={() => setActiveTab(isOpen ? null : tab.id)}
                                 className="w-full flex items-center justify-between"
                             >
-                                <span className={`text-n4100 ${isOpen ? 'text-n7100' : ''} btn-m`}>{tab.label}</span>
+                                <span
+                                    className={`text-n4100 ${isOpen ? 'text-n7100' : ''} btn-m`}
+                                >
+                                    {tab.label}
+                                </span>
 
                                 <Icon
                                     name="ChevronDown"
                                     spanClassName="size-6 2xl:hidden"
                                     iconClassName={`
-                                        w-[12px] h-[6px]
-                                        ${isOpen ? 'rotate-180' : 'rotate-0'}
-                                        transition-all duration-150`}
+                                    w-[12px] h-[6px]
+                                    ${isOpen ? 'rotate-180' : 'rotate-0'}
+                                    transition-all duration-150`}
                                 />
                             </button>
                         </div>
