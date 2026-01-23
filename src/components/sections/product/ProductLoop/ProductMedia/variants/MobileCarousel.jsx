66f92034523f55d7e86d7cfc06c5a7dad8f3d5f2
najijrwan <najijrwanyt@gmail.com>
2026-01-23 21:07:38 +0200

    .

--- src/components/sections/product/ProductLoop/ProductMedia/variants/MobileCarousel.jsx
+++ src/components/sections/product/ProductLoop/ProductMedia/variants/MobileCarousel.jsx
@@ -10,12 +10,12 @@ const MobileCarousel = ({ images = [] }) => {
     });
 
     return (
-        <div className="relative overflow-clip" {...swipeHandlers}>
+        <div className="relative overflow-hidden" {...swipeHandlers}>
             <div
                 className="
-                w-full h-[414px] 
-                flex
-                bg-n2100
+                w-[311px] h-[414px]
+                flex items-center
+                bg-n2100 
                 transition-transform duration-500 ease-out"
                 style={{ transform: `translateX(-${activeIndex * 100}%)` }}
             >
@@ -24,27 +24,51 @@ const MobileCarousel = ({ images = [] }) => {
                         key={i}
                         src={img}
                         alt={`Product image ${i + 1}`}
-                        className="w-full h-full object-cover flex-shrink-0"
+                        className={`w-full ${i === 0 ? 'h-[360px]' : 'h-full'} object-cover flex-shrink-0`}
                     />
                 ))}
             </div>
 
             <Badges variant='large' containerClass='left-6 top-6' />
 
-            {/* Optional buttons */}
             <button
                 disabled={!hasPrev}
                 onClick={handlePrev}
-                className="absolute left-4 top-1/2 -translate-y-1/2 p-2 bg-white rounded-full"
+                className={`
+                        absolute left-6 top-1/2 -translate-y-1/2
+                        size-10
+                        flex items-center justify-center
+                        bg-white rounded-full
+                        ${!hasPrev ? 'cursor-not-allowed' : ''}
+                    `}
             >
-                <Icon name="ArrowLeft" spanClassName="size-6" />
+                <Icon
+                    name='ArrowLeft'
+                    spanClassName='size-6'
+                    iconClassName={`
+                        w-[14px] h-[12px] shadow-1
+                        ${!hasPrev ? 'text-n4100' : 'text-7100'}`}
+                />
             </button>
+
             <button
                 disabled={!hasNext}
                 onClick={handleNext}
-                className="absolute right-4 top-1/2 -translate-y-1/2 p-2 bg-white rounded-full"
+                className={`
+                        absolute right-6 top-1/2 -translate-y-1/2
+                        size-10
+                        flex items-center justify-center
+                        bg-white rounded-full
+                        ${!hasNext ? 'cursor-not-allowed' : ''}
+                    `}
             >
-                <Icon name="ArrowRight" spanClassName="size-6" />
+                <Icon
+                    name='ArrowRight'
+                    spanClassName='size-6'
+                    iconClassName={`
+                        w-[14px] h-[12px] shadow-1
+                        ${!hasNext ? 'text-n4100' : 'text-n7100'}`}
+                />
             </button>
         </div>
     );
