06ecd990c3e8f702e0e8b66cc38d5171a34ed17a
najijrwan <najijrwanyt@gmail.com>
2026-01-23 13:57:43 +0200

    .

--- src/components/sections/product/ProductLoop/ProductMedia/variants/DesktopGrid.jsx
+++ src/components/sections/product/ProductLoop/ProductMedia/variants/DesktopGrid.jsx
@@ -6,18 +6,18 @@ const DesktopGrid = ({ images }) => {
             {images.map((image, i) => (
                 <div
                     key={i}
-                    className='relative w-[262px] h-[349px] bg-n2100'
+                    className='relative w-[262px] h-[349px] bg-n2100 flex items-center'
                 >
                     <img
                         src={image}
                         alt=""
-                        className={`size-full  object-cover`}
+                        className={`w-full ${i === 0 ? 'h-78' : 'h-full'} object-cover`}
                     />
 
                     {i === 0
-                        ?   <Badges
-                                variant='large' 
-                                containerClass='left-4 top-4'/>
+                        ? <Badges
+                            variant='large'
+                            containerClass='left-4 top-4' />
                         : ''}
                 </div>
             ))}
