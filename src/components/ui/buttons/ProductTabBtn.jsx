8851e4f1b45ec4dd0f7610ae4c17fa308d574666
najijrwan <najijrwanyt@gmail.com>
2026-01-27 19:54:03 +0200

    .

--- src/components/ui/buttons/ProductTabBtn.jsx
+++ src/components/ui/buttons/ProductTabBtn.jsx
@@ -1,39 +1,36 @@
 import { Icon } from '@ui';
-import { PRODUCT_TABS } from './ProductTabs.config';
 
-const ProductTabBtn = ({ onClick }) => {
+const ProductTabBtn = ({ tab, isOpen, onClick }) => {
     return (
-        PRODUCT_TABS.map((tab) => (
-            <div
-                key={tab.id}
-                className={`
-                    pb-2 @2xl:pb-0
-                    ${isOpen
-                        ? 'border-b border-b-n7100'
-                        : 'border-b border-b-n4100 @2xl:border-0'}
-                `}
+        <div
+            key={tab.id}
+            className={`
+                pb-2 @2xl:pb-0
+                ${isOpen
+                    ? 'border-b border-b-n7100'
+                    : 'border-b border-b-n4100 @2xl:border-0'}
+            `}
+        >
+            <button
+                onClick={() => onClick(tab.id)}
+                className="w-full flex items-center justify-between"
             >
-                <button
-                    onClick={onClick}
-                    className="w-full flex items-center justify-between"
+                <span
+                    className={`text-n4100 ${isOpen ? 'text-n7100' : ''} btn-m`}
                 >
-                    <span
-                        className={`text-n4100 ${isOpen ? 'text-n7100' : ''} btn-m`}
-                    >
-                        {tab.label}
-                    </span>
+                    {tab.label}
+                </span>
 
-                    <Icon
-                        name="ChevronDown"
-                        spanClassName="size-6 @2xl:hidden"
-                        iconClassName={`
-                            w-[12px] h-[6px]
-                            ${isOpen ? 'rotate-180' : 'rotate-0'}
-                            transition-all duration-150`}
-                    />
-                </button>
-            </div>
-        ))
+                <Icon
+                    name="ChevronDown"
+                    spanClassName="size-6 @2xl:hidden"
+                    iconClassName={`
+                        w-[12px] h-[6px]
+                        ${isOpen ? 'rotate-180' : 'rotate-0'}
+                        transition-all duration-150`}
+                />
+            </button>
+        </div>
     );
 };
 
