55e7867659a872bd15995a43cad9dabbd009609c
najijrwan <najijrwanyt@gmail.com>
2026-01-28 13:09:20 +0200

    ز

--- src/components/sections/product/ProductLoop/ProductLoop.jsx
+++ src/components/sections/product/ProductLoop/ProductLoop.jsx
@@ -1,17 +1,6 @@
 import { ProductMedia, ProductInfo, } from '@product';
-import { useBreakpoint } from '@hooks';
-
-const PRODUCT_IMAGES = [
-    "src/assets/images/product image 1 black.png",
-    "src/assets/images/product image 1.1.png",
-    "src/assets/images/product image 1.2.png",
-    "src/assets/images/product image 1.3.png",
-    "src/assets/images/product image 1.4.png",
-    "src/assets/images/product image 1.5.png",
-]
 
 const ProductLoop = () => {
-    const { isMobile } = useBreakpoint();
     return (
         <section
             className="
@@ -19,12 +8,9 @@ const ProductLoop = () => {
                 flex flex-col 2xl:flex-row 2xl:justify-between"
         >
 
-            <ProductMedia
-                images={PRODUCT_IMAGES}
-                isMobile={isMobile}
-            />
+            <ProductMedia />
 
-            <ProductInfo isMobile={isMobile} />
+            <ProductInfo />
         </section>
     );
 }
--- src/components/sections/product/ProductLoop/ProductMedia/ProductMedia.jsx
+++ src/components/sections/product/ProductLoop/ProductMedia/ProductMedia.jsx
@@ -1,15 +1,27 @@
 import ProductDesktopGrid from './ProductDesktopGrid';
 import { MediaCarousel } from '@ui';
 import { useProduct } from '@product';
+import { useBreakpoint } from '@hooks';
 
-const ProductMedia = ({ images, isMobile }) => {
+const PRODUCT_IMAGES = [
+    "src/assets/images/product image 1 black.png",
+    "src/assets/images/product image 1.1.png",
+    "src/assets/images/product image 1.2.png",
+    "src/assets/images/product image 1.3.png",
+    "src/assets/images/product image 1.4.png",
+    "src/assets/images/product image 1.5.png",
+]
+
+const ProductMedia = () => {
     const { canShowRecommendations } = useProduct();
+    const { isMobile } = useBreakpoint();
 
-    if (canShowRecommendations && !isMobile) return <ProductDesktopGrid images={images} />;
+    if (canShowRecommendations && !isMobile)
+        return <ProductDesktopGrid images={PRODUCT_IMAGES} />;
 
     return (
         <MediaCarousel
-            images={images}
+            images={PRODUCT_IMAGES}
             variant='productImages'
             showBadges={true}
             showThumbs={!isMobile}
