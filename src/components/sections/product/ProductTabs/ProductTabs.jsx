9f2dab175c70281ab1b9055778e263bd0a93f8da
najijrwan <najijrwanyt@gmail.com>
2026-01-28 13:16:54 +0200

    .

--- src/components/sections/product/ProductTabs/ProductTabs.jsx
+++ src/components/sections/product/ProductTabs/ProductTabs.jsx
@@ -1,24 +1,16 @@
 import { useState } from 'react';
-import { useProduct } from '@product';
 import ProductTabsInline from './variants/ProductTabsInline';
 import ProductTabsSection from './variants/ProductTabsSection';
 import { PRODUCT_TABS } from './ProductTabs.config';
-import { useBreakpoint } from '@hooks';
 
-const ProductTabs = ({ }) => {
-    const { canShowRecommendations } = useProduct();
-    const { isMobile } = useBreakpoint();
-
+const ProductTabs = ({ variant }) => {
     const [activeTab, setActiveTab] = useState(null);
 
     const handleTabClick = (tabId) => {
         setActiveTab(activeTab === tabId ? null : tabId);
     }
 
-    console.log('can:', canShowRecommendations)
-    console.log('mobile: ', isMobile);
-    
-    if (canShowRecommendations || isMobile)
+    if (variant === 'inline')
         return <ProductTabsInline
             tabs={PRODUCT_TABS}
             activeTab={activeTab}
