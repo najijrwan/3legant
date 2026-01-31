553a6bb31035c290b455aa854c94475bf45136d5
najijrwan <najijrwanyt@gmail.com>
2026-01-31 08:40:42 +0200

    .

--- src/components/sections/shop/PageHeader.jsx
+++ src/components/sections/shop/PageHeader.jsx
@@ -2,12 +2,15 @@ import { Icon } from '@ui';
 
 const PageHeader = ({ title }) => {
     return (
-        <section className='relative px-8 2xl:px-40'>
-            <img
-                src="src/assets/images/shop hero image.png"
-                alt=""
-                className="w-full h-[392px] object-cover object-center"
-            />
+        <section className='px-8 2xl:px-40'>
+            <div
+                className='
+                w-full h-[308px] 2xl:h-[392px]
+                flex items-center justify-center gap-4 2xl:gap-6
+                bg-[url("src/assets/images/shop-hero-image.png")] bg-cover bg-center'
+            >
+
+            </div>
         </section>
     )
 }
